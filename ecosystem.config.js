32ca8c23b4b10dead41474eb2135c5bdf3a4e570
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2021-09-24 17:50:29 -0300

    feat(eslint): add eslint and .gitignore

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    apps : [
-        {
-            name: 'Discrod BOT - WEB DJ ',
-            script: 'npm start',
+  apps: [
+    {
+      name: 'Discrod BOT - WEB DJ ',
+      script: 'npm start',
 
-            // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '1G'
-        }
-    ]
-};
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G'
+    }
+  ]
+}
